37d85218b74bc6a7991778f9f26e4992ee734353
zivd <ziad99kr@gmail.com>
2024-11-23 10:53:01 +0100

    firstttt

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Map  from '/src/maps/Map'
+import './App.css'
+import './index.css'
+function App() {
+ 
+
+  return (
+    <>
+ <Map />
+
+    </>
+  )
+}
+
+export default App
