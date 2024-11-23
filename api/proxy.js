02a7f51e738aea2185ff10d9c9d621567d27f096
Dymirt <kolidadmytro@gmail.com>
2024-11-23 15:31:22 +0100

    wtf
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- api/proxy.js
+++ api/proxy.js
@@ -1,17 +1,23 @@
+// api/proxy.js
 import axios from 'axios';
 
 export default async function handler(req, res) {
   const { resource_id, limit } = req.query;
 
   try {
+    console.log('Fetching data from external API...');
     const response = await axios.get('https://api.um.warszawa.pl/api/action/datastore_search', {
       params: {
         resource_id,
         limit,
       },
+      timeout: 10000, // Increase timeout to 10 seconds
     });
+
+    console.log('Data fetched from external API:', response.data);
     res.status(200).json(response.data);
   } catch (error) {
+    console.error('Error fetching data from external API:', error);
     res.status(error.response?.status || 500).json({ error: error.message });
   }
 }
